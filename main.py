08285808ca2035f909668dd28f4eaa8fde8777ce
Salabim-mb <m.balas1504@gmail.com>
2021-06-15 13:38:08 +0200

    poprawa wspólbieznosci

--- main.py
+++ main.py
@@ -3,65 +3,75 @@ from ant_colony import AntColony
 from config import Config
 import time
 import parallelize as pl
-import copy
 
+############## STAŁE ####################
 
 MAP_FILENAME = 'map.json'
 CONFIG_FILENAME = 'config.json'
 RANDOM_MAP_SAVE_FILENAME = 'randmap.json'
 
-config = Config()
-config.read_config_from_file(CONFIG_FILENAME)
-
-places_map = None
-if not config.random_map:
-    map_reader = MapReader(MAP_FILENAME)
-    places_map = map_reader.read_map()
-else:
-    places_map = config.randomize_map()
-
+#########################################
 
 def main():
     
     # inicjalizacja MPI i przygotowanie środowiska
     comm, rank, num_proc = pl.init_mpi()
-    best_route = None
-    colony = AntColony(places_map, config.alpha, config.beta, config.decay)
-    
-    # podział liczby mrówek na porcje wysyłane do wątków
+    places_map = None
+    config = Config()
+    config.read_config_from_file(CONFIG_FILENAME)
+    all_routes = None
+
+    # podział liczby mrówek na porcje wysyłane do procesów
     ants = pl.divide_ants(config.ants, num_proc)
 
-    # wątek główny - czas start
+    # proces główny - init
     if rank == 0:
+        if not config.random_map:
+            map_reader = MapReader(MAP_FILENAME)
+            places_map = map_reader.read_map()
+        else:
+            places_map = config.randomize_map()
+        colony = AntColony(places_map, config.alpha, config.beta, config.decay)
+
         calc_start_time = time.time()
 
-    # wszystkie wątki - właściwa sekcja algorytmu
+    # wszystkie procesy - właściwa sekcja algorytmu
     for i in range(config.iterations):
         i_routes = []
 
-        # wątek główny - przygotowanie iteracji
+        # proces główny - przygotowanie iteracji
         if rank == 0:
             print("Performing iteration:", i)
             colony.evaporate_pheromones()
-            pl.scatter_data(comm, ants)
+            pl.send_data(comm, num_proc, colony)
+        # procesy poboczne - przypisanie właściwej kolonii
+        else:
+            colony = pl.recv_data(comm)
 
-        # wszystkie wątki - poszukiwanie najlepszej drogi
-        ants_subgroup = int(pl.get_ants(comm, ants, rank))
-        subcolony = copy.deepcopy(colony)
+        # wszystkie procesy - poszukiwanie najlepszej drogi
+        ants_subgroup = int(ants[rank])
         for j in range(ants_subgroup):
-            route = subcolony.find_new_route()
-            # lengths are under index 1
+            route = colony.find_new_route()
+            # długości ścieżek są pod indeksem 1
+            i_routes.append(route)
+        
+        # wyniki częściowe po iteracji
+        gathered_routes = pl.gather_data(comm, i_routes)
+        if rank == 0:
+            all_routes = []
+            for l in gathered_routes:
+                all_routes += l
+            colony.leave_pheromones(all_routes)
+
+    # proces główny - agregacja wyników
+    if rank == 0:
+
+        # bierzemy pod uwagę ostatnią iterację
+        best_route = None
+        for route in all_routes:
             if not best_route or route[1] < best_route[1]:
                 best_route = route
-            i_routes.append(route[0])
-        
-    # wyniki częściowe po iteracji
-    gathered_routes = pl.gather_data(comm, i_routes)
-    if rank == 0:
-        subcolony.leave_pheromones(gathered_routes)
 
-    # wątek główny - agregacja wyników
-    if rank == 0:
         best_route_str = list(map(lambda id: places_map.places[id], best_route[0]))
         calc_time_elapsed = time.time() - calc_start_time
         print('Best route:')
@@ -70,8 +80,8 @@ def main():
         print()
         print("Elapsed time: %.3f seconds" % calc_time_elapsed)
 
-    if config.rand_places and config.save_generated_map:
-        config.save_generated_map_to_file(RANDOM_MAP_SAVE_FILENAME, places_map)
+        if config.rand_places and config.save_generated_map:
+            config.save_generated_map_to_file(RANDOM_MAP_SAVE_FILENAME, places_map)
 
 
 main()
--- parallelize.py
+++ parallelize.py
@@ -9,30 +9,41 @@ def init_mpi() -> Any:
     return comm, comm.Get_rank(), comm.Get_size()
 
 
-# funkcja: przydział liczby mrówek do każdego z wątków
+# funkcja: przydział liczby mrówek do każdego z procesów
 def divide_ants(num_ants: int, num_proc: int) -> list:
     blocks = []
     block_size =  ceil(num_ants / num_proc)
     current_idx = 0
     
     while (current_idx < num_ants):
-        blocks.append(min((num_ants - current_idx), block_size)) # zapewnia że ostatnia wartość dostanie modulo
+        blocks.append(min((num_ants - current_idx), block_size)) # zapewnia że w przypadku niepodzielności ostatnia wartość dostanie modulo
         current_idx += block_size
 
     return blocks
     
 
-# funkcja: rozdzielenie danych pomiędzy wątki
-def scatter_data(comm: Any, blocks: list[int], root_rank=0) -> None: # root_rank na wypadek, gdybyśmy chcieli zmienić głównego workera na jeden z wątków potomnych
+# funkcja: rozdzielenie danych pomiędzy procesy
+def scatter_data(comm: Any, blocks: list[int], root_rank=0) -> None: # root_rank na wypadek, gdybyśmy chcieli zmienić głównego workera na jeden z procesów potomnych
     comm.scatter(blocks, root=root_rank)
 
 
-# funkcja odbierająca mrówki w wątku potomnym
+# funkcja odbierająca mrówki w procesie potomnym
 def get_ants(comm: Any, blocks: list[int], current_rank: int, root_rank=0) -> int:
     current_ants = comm.scatter(blocks, root=root_rank)
     return current_ants
 
 
-# funkcja zbierająca wszystkie dane w wątku głównym
+# funkcja zbierająca wszystkie dane w procesie głównym
 def gather_data(comm: Any, ant_product: Any, root_rank=0) -> Any:
     return comm.gather(ant_product, root=root_rank)
+
+
+# funkcja imitująca broadcast przez komunikację blokującą
+def send_data(comm: Any, num_proc: int, data: Any) -> None:
+    for dest in range(1, num_proc):
+        comm.send(data, dest=dest)
+
+
+# funkcja odbierająca dane dla konkretnego procesu
+def recv_data(comm: Any, root_rank=0) -> Any:
+    return comm.recv(source=root_rank)
